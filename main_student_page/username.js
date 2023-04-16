3fb3ad09daa9b60c803f08aa0a2d008ae9cbfc5f
Divyansh Gupta <divyanshgupta1811@gmail.com>
2023-04-16 17:08:52 +0530

    10th commot

---
+++ main_student_page/username.js
@@ -0,0 +1,6 @@
+function save(){
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('pass').value;    
+}
+export{name,email,password}
