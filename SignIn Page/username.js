fa54e866bbcd34c8ff3b2d303da09484e2449db6
Divyansh Gupta <divyanshgupta1811@gmail.com>
2023-04-16 16:02:21 +0530

    7th commit

---
+++ SignIn Page/username.js
@@ -0,0 +1,5 @@
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('pass').value;    
+
+export{name}
---
+++ main_student_page/frame1.js
@@ -0,0 +1,6 @@
+import {name, email} from './';
+const user_name=document.getElementById('user_name')
+user_name.innerText=name
+
+
+
